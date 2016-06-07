2988fcc4594738fe6d2735f084ba96a96c730e0e
RAHAT <RAHAT@gmail.com>
2016-06-07 11:40:30 -0400

    Bassy

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^user/(?P<pk>\d+)/$', views.user_post_list, name='user_post_list'),
 ]
